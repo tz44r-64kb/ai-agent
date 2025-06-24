ac403a3c9624ea7d5269ac3bbcfdefa0b34c037d
tz44r-64kb <tz44r.64kb@gmail.com>
2025-06-24 15:16:10 +0200

    refactor main

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import os
 import sys
 from dotenv import load_dotenv
-args = sys.argv[1:]
-
-if not args:
-    print("AI Code Assistant")
-    print('\nUsage: python main.py "your prompt here"')
-    print('Example: "python3 main.py "How do I build a calculator app?"')
-    sys.exit(1)
-
-prompt = " ".join(args)
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
+from google.genai import types
 from google import genai
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    args = sys.argv[1:]
 
-response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents=prompt
-        )
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: "python3 main.py "How do I build a calculator app?"')
+        sys.exit(1)
 
-prompt_token_count = response.usage_metadata.prompt_token_count
-candidates_token_count = response.usage_metadata.candidates_token_count
+    prompt = " ".join(args)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-print(response.text)
-print(f"Prompt tokens: {prompt_token_count}")
-print(f"Response tokens: {candidates_token_count}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),        
+    ]
+
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=messages
+            )
+
+    prompt_token_count = response.usage_metadata.prompt_token_count
+    candidates_token_count = response.usage_metadata.candidates_token_count
+
+    print(response.text)
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
