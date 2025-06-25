c1dd9ce23cdbb66bf1e08e85dbc10a570fcfb8f1
tz44r-64kb <tz44r.64kb@gmail.com>
2025-06-25 21:23:32 +0200

    introduce verbose option

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: "python3 main.py "How do I build a calculator app?"')
         sys.exit(1)
-
+    
+    is_verbose = "--verbose" in args
     prompt = " ".join(args)
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -23,9 +24,12 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)]),        
     ]
 
-    generate_content(client, messages)
+    if is_verbose:
+        print(f"User prompt: {prompt}")
 
-def generate_content(client, messages):
+    generate_content(client, messages, is_verbose)
+
+def generate_content(client, messages, is_verbose):
     response = client.models.generate_content(
             model="gemini-2.0-flash-001", 
             contents=messages
@@ -35,8 +39,10 @@ def generate_content(client, messages):
     candidates_token_count = response.usage_metadata.candidates_token_count
 
     print(response.text)
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {candidates_token_count}")
+    if is_verbose:
+        print(f"Prompt tokens: {prompt_token_count}")
+        print(f"Response tokens: {candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
